2587d8321242824de021123df373c737a44b4382
Ed Gracia <talogracia@gmail.com>
2026-05-31 20:16:56 -0500

    added analytics support

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import DotGrid from "@/components/DotGrid";
 import PageTransition from "@/components/PageTransition";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
